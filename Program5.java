04d7dce8dbdcadf61101aff85965ffb579970496
sharan.ka <sharan.ka@zohocorp.com>
2022-08-15 18:48:42 +0530

    modulo operation - recursive

---
+++ Program5.java
@@ -0,0 +1,20 @@
+// Modulo operation
+
+public class Program5 {
+    public static int recursive(int dividend, int divisor) {
+        if (divisor == 0) {
+            return -1;
+        }
+
+        // Base case
+        if (dividend < divisor) {
+            return dividend;
+        } else { // Recursive case
+            return recursive(dividend - divisor, divisor);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recursive(15, 4));
+    }
+}
